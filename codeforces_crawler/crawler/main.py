a01473e6068640092c55b47af3e80d2dbeb13ff8
dristiro@iitg.ac.in <dristirons@gmail.com>
2020-03-19 14:41:49 +0530

    Added Submission By Rating

--- codeforces_crawler/crawler/main.py
+++ codeforces_crawler/crawler/main.py
@@ -3,6 +3,8 @@ import requests
 import re
 import json
 import datetime, time
+from collections import Counter
+import numpy as np
 
 # def profile(username):
 #     source = requests.get('https://codeforces.com/profile/'+username).text
@@ -21,10 +23,9 @@ def crawler_1(users):
         api_link += users[j]
         i += 1
         
-    print(api_link)
-    request_object = requests.get(api_link).text
-    json_data = json.loads(request_object)
-    result = json_data['result']
+    #print(api_link)
+    load = json.loads(requests.get(api_link).text)
+    result = load['result']
     username = []
     dp = []
     rank = []
@@ -36,9 +37,8 @@ def crawler_1(users):
 
 def crawler_2(user):
     api_link_1 = 'https://codeforces.com/api/user.rating?handle={}'.format(user)
-    request_object = requests.get(api_link_1).text
-    json_data = json.loads(request_object)
-    result = json_data['result']
+    load = json.loads(requests.get(api_link_1).text)
+    result = load['result']
     user_rating = []
     # label = []
     for item in result:
@@ -52,3 +52,41 @@ def crawler_2(user):
         temp['ratingChange'] = item['newRating']-item['oldRating']
         user_rating.append(temp)
     return (user_rating)
+
+def crawler_3(user):
+    api_link = 'https://codeforces.com/api/user.status?handle={}&from=1&count=6000'.format(user)
+    load = json.loads(requests.get(api_link).text)
+    result = load['result']
+    virtual = []
+    contest = []
+    unofficial = []
+    practice = []
+    for item in result:
+        if item['verdict'] != 'OK' or 'rating' not in item['problem']:
+            continue
+        if item['author']['participantType'] == 'VIRTUAL':
+            virtual.append(item['problem']['rating'])
+        elif item['author']['participantType'] == 'CONTESTANT':
+            contest.append(item['problem']['rating'])
+        elif item['author']['participantType'] == 'OUT_OF_COMPETITION':
+            unofficial.append(item['problem']['rating'])
+        else :
+            practice.append(item['problem']['rating'])
+
+    virtual = np.bincount(np.array(virtual).astype(np.int64))
+    contest = np.bincount(np.array(contest).astype(np.int64))
+    unofficial = np.bincount(np.array(unofficial).astype(np.int64))
+    practice = np.bincount(np.array(practice).astype(np.int64))
+    for i in range(len(virtual), 4000):
+        np.append(virtual,0)
+    for i in range(len(contest), 4000):
+        np.append(contest,0)
+    for i in range(len(unofficial), 4000):
+        np.append(unofficial, 0)
+    for i in range(len(practice), 4000):
+        np.append(practice, 0)
+    print(virtual[1500])
+    print(contest[1500])
+    print(unofficial[1500])
+    print(practice[1500])
+    return (virtual, contest, unofficial, practice)
--- codeforces_crawler/crawler/views.py
+++ codeforces_crawler/crawler/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from codeforces_crawler.forms import UserIDForm, CompareIDForm
-from .main import crawler_1, crawler_2
+from .main import crawler_1, crawler_2, crawler_3
 import json
+from collections import Counter
+import numpy as np
 # Create your views here.
 
 def login(request):
@@ -13,7 +15,43 @@ def login(request):
             user_rating = crawler_2(str(user))
             dicti = {'user_rating': user_rating, 'username':username[0], 'dp': dp[0] ,'legendary_grandmaster': False, 'international_grandmaster': False, 'grandmaster': False, 'international_master': False, 'master': False, 'candidate_master': False, 'expert': False,'specialist': False, 'pupil': False, 'newbie':False}
             dicti[rank[0].replace(' ','_')] = True
-            return render(request, 'crawler/index.html', dicti)
+            virtual, contest, unofficial, practice = crawler_3(str(user))
+            temp_a = []
+            for i in range(len(virtual)):
+                temp1 = {}
+                temp1['label'] = i
+                temp1['y'] = virtual[i]
+                temp_a.append(temp1)
+            dicti['virtual'] = temp_a
+            #print(dicti['virtual'][1500])
+            temp_b = []
+            for i in range(len(contest)):
+                temp1 = {}
+                temp1['x'] = i
+                temp1['y'] = contest[i]
+                temp_b.append(temp1)
+            dicti['contest'] = temp_b
+            #print(dicti['contest'][1500])
+            #temp.clear()
+            temp_c = []
+            for i in range(len(unofficial)):
+                temp1 = {}
+                temp1['x'] = i
+                temp1['y'] = unofficial[i]
+                temp_c.append(temp1)
+            dicti['unofficial'] = temp_c
+            #print(dicti['unofficial'][1500])
+            #temp.clear()
+            temp_d = []
+            for i in range(len(practice)):
+                temp1 = {}
+                temp1['x'] = i
+                temp1['y'] = practice[i]
+                temp_d.append(temp1)
+            dicti['practice'] = temp_d
+            #print(dicti['practice'][1500])
+            #print(dicti)
+            return render(request, 'crawler/index.html', context = dicti)
     form = UserIDForm()
     return render(request,'crawler/login.html', {'form':form})
 
@@ -34,7 +72,7 @@ def compare(request):
             users.append(dicti)
             user_rating = crawler_2(str(user2))
             dicti = {'user_rating': user_rating, 'username':username[1], 'dp': dp[1] ,'legendary_grandmaster': False, 'international_grandmaster': False, 'grandmaster': False, 'international_master': False, 'master': False, 'candidate_master': False, 'expert': False,'specialist': False, 'pupil': False, 'newbie':False}
-            dicti[rank[1].replace(' ','_ ')] = True
+            dicti[rank[1].replace(' ','_')] = True
             users.append(dicti)
             return render(request, 'crawler/compare.html', {'users': users})
     form = CompareIDForm()
